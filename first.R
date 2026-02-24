476327a80de5ed404b16f2f43d43725de93108a0
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2026-02-24 19:31:17 +0545

    file uploaded

---
+++ first.R
@@ -0,0 +1,27 @@
+x <- 10
+print(x)
+y <- 16
+
+x + y
+x - y
+x * y
+y / x
+y %/% x  #Integer division
+y %% x  #Modulus 
+y ^ x  #exponent
+
+2%*%5
+2%o%5
+
+y == 16
+x != 10
+
+a <- 0
+b <- 2 
+
+print (a&b)
+print (a&&b)
+print (a|b)
+print (a||b)
+
+
