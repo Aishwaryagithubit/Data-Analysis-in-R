011ee32e5ce6190dfc965ba08782c089dc818e0f
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2026-05-04 22:26:34 +0545

    Add files via upload

---
+++ Fraud.R
@@ -0,0 +1,39 @@
+---
+title: "Exploratory Data Analysis"
+author: "Student Name: Aishwarya Sah"
+date: "2026-05-01"
+---
+library(ggplot2)  
+# Load dataset
+data <- read.csv("C:/Users/ASUS/Desktop/RProgram/Frauddata.csv")
+
+# View first few rows to understand structure
+head(data)
+
+# Structure of dataset
+str(data)
+
+# Summary statistics
+summary(data)
+
+# Dimensions
+dim(data)
+
+#Count missing values in each column
+colSums(is.na(data))
+
+# Count number of fraud and non-fraud cases
+table(data$isFraud)
+
+# Calculate proportion of each class
+prop.table(table(data$isFraud))
+
+# Visualise class imbalance
+ggplot(data, aes(x = as.factor(isFraud))) +
+  geom_bar() +
+  labs(title = "Distribution of Fraud vs Non Fraud Transactions",
+       x = "Fraud (0 = Legitimate, 1 = Fraud)",
+       y = "Count")
+
+
+
